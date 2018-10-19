fc9b2d190c97db187dcfb4d909021ade82c8e1f5
Günter Hertz <gunter.mhertz@gmail.com>
2018-10-19 00:00:49 -0300

    Create project

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Günter Hertz on 18/10/18.
+//  Copyright © 2018 Günter Hertz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
