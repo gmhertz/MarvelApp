aee76bf62b4e2834af8330a2e4ba33b14ff7420c
Günter Hertz <gunter.mhertz@gmail.com>
2018-10-21 05:16:08 -0300

    Add color extension file

---
+++ MarvelApp/Services/UIColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColorExtension.swift
+//  MarvelApp
+//
+//  Created by Günter Hertz on 21/10/18.
+//  Copyright © 2018 Günter Hertz. All rights reserved.
+//
+import UIKit
+
+extension UIColor {
+    static let darkBlue = UIColor(red: 35/255, green: 45/255, blue: 71/255, alpha: 1)
+    static let softBlue = UIColor(red: 43/255, green: 55/255, blue: 91/255, alpha: 1)
+}
