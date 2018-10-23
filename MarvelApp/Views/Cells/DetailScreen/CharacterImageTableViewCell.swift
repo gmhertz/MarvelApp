5e87b5801942adab97f382934476179b29adb605
Günter Hertz <gunter.mhertz@gmail.com>
2018-10-23 13:35:48 -0300

    Update character image cell

--- MarvelApp/Views/Cells/DetailScreen/CharacterImageTableViewCell.swift
+++ MarvelApp/Views/Cells/DetailScreen/CharacterImageTableViewCell.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class CharacterImageTableViewCell: UITableViewCell {
-    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var characterImage: UIImageView! {
+        didSet {
+            characterImage.clipsToBounds = true
+            characterImage.layer.borderWidth = 3.0
+            characterImage.layer.borderColor = UIColor.white.cgColor
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,8 +26,8 @@ class CharacterImageTableViewCell: UITableViewCell {
     }
     
     func setup(thumbnail: Thumbnail){
-        let url = thumbnail.fullPath(variant: .landscape_medium)
-        self.imageView?.getImageFromURL(url: url)
+        let url = thumbnail.fullPath(variant: .landscape_large)
+        self.characterImage?.getImageFromURL(url: url)
     }
     
 }
