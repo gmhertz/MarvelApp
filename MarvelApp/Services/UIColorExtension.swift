5c45b1cf891c2ac450c4e0135846a09f740fbc82
Günter Hertz <gunter.mhertz@gmail.com>
2018-10-23 01:23:32 -0300

    Update color extension file with marvel red

--- MarvelApp/Services/UIColorExtension.swift
+++ MarvelApp/Services/UIColorExtension.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UIColor {
-    static let darkBlue = UIColor(red: 35/255, green: 45/255, blue: 71/255, alpha: 1)
+    static let darkBlue = UIColor(red: 35/255, green: 45/255, blue: 71/255, alpha: 1.0)
     static let softBlue = UIColor(red: 43/255, green: 55/255, blue: 91/255, alpha: 1)
+    static let marvelRed = UIColor(red: 226/255, green: 54/255, blue: 54/255, alpha: 1)
 }
