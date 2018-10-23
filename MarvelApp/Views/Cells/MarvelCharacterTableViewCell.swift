302b1807b246c5cbd522e2d8251a448c1e61deef
Günter Hertz <gunter.mhertz@gmail.com>
2018-10-23 01:22:46 -0300

    Update behavior marvel character table view cell
    Need to implement to show data

--- MarvelApp/Views/Cells/MarvelCharacterTableViewCell.swift
+++ MarvelApp/Views/Cells/MarvelCharacterTableViewCell.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class MarvelCharacterTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var detailView: UIView!
+    @IBOutlet weak var characterImage: UIImageView! {
+        didSet {
+            characterImage.layer.cornerRadius = characterImage.frame.size.height/2
+            characterImage.clipsToBounds = true
+            characterImage.layer.borderWidth = 3.0
+            characterImage.layer.borderColor = UIColor.marvelRed.cgColor
+        }
+    }
+    @IBOutlet weak var characterName: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        UIView.animate(withDuration: 0.5) {
+            self.detailView.alpha = 0.5
+            self.detailView.alpha = 1.0
+        }
+    }
+    
+    func setup(with name: String, thumbnail: Thumbnail) {
+        self.characterName.text = name
+        let imageUrl = thumbnail.fullPath(variant: .landscape_medium)
+        self.characterImage.getImageFromURL(url: imageUrl)
     }
     
 }
