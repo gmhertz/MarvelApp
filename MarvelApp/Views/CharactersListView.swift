5dc0608f5bfa6fd3cb3bf5415a9112271eaede28
Günter Hertz <gunter.mhertz@gmail.com>
2018-10-23 01:24:53 -0300

    Implement first screen
    Its basically functional, need some design updates

--- MarvelApp/Views/CharactersListView.swift
+++ MarvelApp/Views/CharactersListView.swift
@@ -16,6 +16,9 @@ class CharactersListView: UIViewController {
     @IBOutlet weak private var charactersTableView: UITableView! {
         didSet {
             charactersTableView.register(UINib(nibName: "CharacterTableViewCell", bundle: nil), forCellReuseIdentifier: "characterCell")
+            
+            charactersTableView.register(UINib(nibName: "MarvelCharacterTableViewCell", bundle: nil), forCellReuseIdentifier: "marvelCharacterCell")
+//            charactersTableView.register(MarvelCharacterTableViewCell.self, forCellReuseIdentifier: "marvelCharacterCell")
         }
     }
     
@@ -24,16 +27,20 @@ class CharactersListView: UIViewController {
     let disposeBag: DisposeBag = DisposeBag()
     var characterDetails = PublishSubject<MarvelCharacter>()
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.title = "Characters List"
+        self.configure()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Characters List"
-        self.navigationController?.navigationBar.barStyle = .black
         
         charactersTableView.rx.setDelegate(self).disposed(by: disposeBag)
         
         let dataSource = RxTableViewSectionedReloadDataSource<SectionOfCharacterDataInfo>(configureCell: { (_, charactersTableView, indexPath, item) -> UITableViewCell in
-            if let cell = charactersTableView.dequeueReusableCell(withIdentifier: "characterCell", for: indexPath) as? CharacterTableViewCell {
-                cell.setup(name: item.name, thumbnail: item.image)
+            if let cell = charactersTableView.dequeueReusableCell(withIdentifier: "marvelCharacterCell", for: indexPath) as? MarvelCharacterTableViewCell {
+                cell.setup(with: item.name, thumbnail: item.image)
                 return cell
             } else {
                 fatalError("ERROR ON CELL LOADING")
@@ -43,22 +50,32 @@ class CharactersListView: UIViewController {
         viewModel.data
             .bind(to: charactersTableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
-        
+
         charactersTableView.rx.itemSelected
             .bind(to: viewModel.selectedCharacter)
             .disposed(by: disposeBag)
-        
+
         charactersTableView.rx.didScroll
             .map { [unowned self] _ in self.charactersTableView.isBouncingBottom }
             .bind(to: viewModel.shouldLoadMoreCharacters )
             .disposed(by: disposeBag)
-        
-        
+
+        viewModel.characterToDetail
+            .subscribe(onNext: { character in
+                let detailViewModel = CharacterDetailViewModel(with: character)
+                let characterDetailVC = CharacterDetailView.with(detailViewModel)
+                self.navigationController?.pushViewController(characterDetailVC, animated: true)
+            }).disposed(by: disposeBag)
+    }
+    
+    
+    func configure() {
+        navigationItem.largeTitleDisplayMode = .always
     }
 }
 
 extension CharactersListView: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
+        return 120
     }
 }
